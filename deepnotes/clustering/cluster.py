b738be8940ee4e4fb3f2efd740f3a847964269b4
Jacky <gokurohono@gmail.com>
2025-12-31 20:31:20 +1100

    feat: adding lock capabilities

--- deepnotes/clustering/cluster.py
+++ deepnotes/clustering/cluster.py
@@ -1,4 +1,4 @@
-
+from __future__ import annotations
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Self
@@ -9,8 +9,12 @@ from deepnotes.clustering.file_node import FileNode
 @dataclass
 class Cluster:
     nodes: dict[Path, ClusterNode] = field(default_factory=lambda: dict())
+    lock_: bool = field(default=False)
 
     def add_node(self, node: FileNode) -> Self:
+        if self.lock_:
+            raise RuntimeError("Cannot add node to a locked cluster")
+
         assert isinstance(node, FileNode)
         file_path = node.file_path
 
@@ -22,6 +26,22 @@ class Cluster:
             connections=[],
         )
         return self
+
+
+    # once locked it will be immutable
+    # and you must generate a new cluster.
+    def lock(self):
+        self._lock = True
+
+    def deep_clone(self) -> Cluster:
+        inst = Cluster()
+        for node in self.nodes:
+            inst.add_node(node)
+        return inst
     
+    def calculate(self):
+        if self._lock:
+            raise RuntimeError("Cannot recalculate on a locked cluster.")
+        
 
 
