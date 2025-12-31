84a5243305212e95fe183c207c9c624f51c8ce5d
Jacky <gokurohono@gmail.com>
2025-12-31 20:22:33 +1100

    feat: cluster adds nodes

--- deepnotes/clustering/cluster.py
+++ deepnotes/clustering/cluster.py
@@ -1,13 +1,23 @@
 
 from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Self
 
 from deepnotes.clustering.cluster_node import ClusterNode
+from deepnotes.clustering.file_node import FileNode
 
 @dataclass
 class Cluster:
     nodes: dict[Path, ClusterNode] = field(default_factory=lambda: dict())
 
-
+    def add_node(self, node: FileNode) -> Self:
+        assert isinstance(node, FileNode)
+        file_path = node.file_path
+        self.nodes[file_path] = ClusterNode(
+            file_node=node,
+            connections=[],
+        )
+        return self
     
+
 
